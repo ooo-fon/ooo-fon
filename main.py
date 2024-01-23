17969605e93622395f15ab39340cfd1db0149e8a
ooo-fon <ooo-fon@mail.ru>
2024-01-23 18:37:01 +0500

    12

---
+++ main.py
@@ -0,0 +1,404 @@
+# name = 'Javlon'
+# favorite_number = 7
+# valve_of_pi = 3.14
+#
+# print (name)
+# print (favorite_number)
+# print (valve_of_pi)
+# print(type(2.00))
+# # print(type(2))
+# name = input('Вспомогательный текст')
+# print('Привет' (name))
+
+# name = 'Petr'
+# age = 35
+# temp =  39.4
+# print (name,type (name))
+# print (age,type (age))
+# print (temp,type (temp))
+
+# print (3+6**2)
+# print (7*3**2)
+# print (7//2+0.5)
+# print (3%2*2)
+
+
+#
+#
+# x=7
+# y = 'Как дела'
+# print((x))
+
+
+
+# name = input(' Роман')
+# age = input ('25')
+# result_text = f'Привет {name}как дела {age} сколько лет'
+# print (result_text)
+
+# a = int(input('Число'))
+# if a > 100:
+#     print('a is greator 100')
+# elif a < 100:
+#     print ('a is smaller 100')
+# else:
+#     print ('a is 100')
+
+# first = int(input('Введите первое число '))
+# matem = input('(+-*\) ')
+# second = int(input('Введите второе число '))
+# if matem == '+':
+#     print(first + second)
+# elif matem == '-':
+#     print(first - second)
+# elif matem == '*':
+#     print(first * second)
+# elif matem == '/':
+#     print(first / second)
+# else:
+#     print ('Неверный  мат оператор')
+
+# Player1 = input('Камень,Ножницы,Бумага')
+# Player2 = input('Камень,Ножницы,Бумага')
+#
+# if Player1 == 'Камень' and Player2 == 'Ножницы':
+#     print ('Победил Player1')
+#
+# elif Player1 == 'Ножницы' and Player2 == 'Бумага':
+#     print ('Победил Player1')
+#
+# elif Player1 == 'Бумага' and Player2 == 'Камень':
+#     print ('Победил Player1')
+#
+# elif Player1 == 'Бумага' and Player2 == 'Ножницы':
+#     print ('Победил Player2')
+# elif Player1 == 'Камень' and Player2 == 'Бумага':
+#     print ('Победил Player2')
+# elif Player1 == 'Ножницы' and Player2 == 'Камень':
+#     print ('Победил Player2')
+# else : print('Нечья')
+
+# my_list = [1,'a',2,4.5]
+# for spam in my_list:
+#     print(spam)
+
+
+# my_list = [6,'2',4]
+# for t in my_list:
+#     print(t+2)
+
+
+# my_tuple = [6,'2',4]
+# for i in range(1,100):
+#     print(f'{my_tuple}')
+
+# names = ['Ivan','Pavel','Jordan']
+# while True:
+#     Print ('[]'.format(names[0::2]))
+# ---------------------------------------------------------
+
+# p = [1,2,'Python','TgBot']
+# while 1 in p:
+#     p.append('yes')
+#     p.remove(1)
+#     print(p)
+
+# names = ['Ivan','Pavel','Jordan']
+# while True:
+#     new = input('Кого добавим?')
+#     if new == 'Список':
+#         print(names)
+#     else:
+#         names.append(new)
+#         print (f'{new}добавлен')
+
+
+
+# my = ['2','33','Jordan','Pavel']
+# my2 = []
+
+# nums = [i for i in range (1,11)]
+# chotnie = [num for num in nums if num % 2 ==0]
+# nechotnie = [num for num in nums if num % 2]
+
+
+# my_list = [i for i in range (1,11,2)]
+# print(my_list)
+
+# names = ['Pavel','Sasha','Jordan','Pasha']
+# ansver = [i[-1] for i in names]
+#
+# # for i in names:
+# #     print(i[0])
+# print(ansver)
+
+# nums = [1,2,3]
+# my_list = [i for i in range (1,11) if i in nums]
+# print (my_list)
+
+# nums1 = [i for i in range (1,21)]
+# nums2 = [for i nums1 in nums1 if num % 2 == 0]
+# print nums1
+
+# names = ['Ivan','Pavel','Jordan']
+# while True:
+#     new = input('Введите имя')
+#     if new in names :
+#         print(names)
+#     else:
+#         names.append(new)
+#         print (f'{new}  Успешно добавлен', names)
+
+
+# family = ['Мама','Papa','Brat','sister']
+# indexsister = family.index('sister')
+# dlina = len(family)-1
+# dlina = 3
+# nomer = 0
+# while :
+#     print(i)
+
+# {}.fromkeys('a',1)
+# print ({}.fromkeys('song','Godzilla'))
+#
+# {}.fromkeys('a',1)
+# print ({}.fromkeys(['song'],'Godzilla'))
+
+# .get('key')
+
+#
+# my = dict (name = 'Jordan',
+# job='Developer',age=23)
+# my2 = my.copy()
+# my2 = my.get('name')
+# print(my2)
+
+# names = {'Names',1,2,3,4,5}
+# print(names[(0)])
+
+# nums = {1,2,3,3,4,4,5,6,}
+# print (set(nums))
+
+
+# nums = {'name':'Jordan','age':25,job=:'Developer'}
+# for k, v in nums.items():
+#
+#
+#     print(k,v,j)
+
+# x = input()
+# sklad = {}
+
+# all_products = {'Склад':{}}
+# while True:
+#     menu = ('Что добавим?')
+#         '1- добавить продукт\n '
+#
+# admin = ('Что добавим?')
+# product_name = input('Продукт')
+# product_count = input('Кол-во')
+# all_products['Склад']['Продукт']['Кол-во']
+#  if
+
+
+# deF name ():
+#     a=1
+#     b=2
+#     c=a+b
+# name ()
+#
+# print:(c)
+
+# def spam2(a):
+#     print(a+6)
+# spam2(3)
+
+# def spam2(a,b,c=7):
+#         print(a+b+c)
+# spam2(3,5)
+
+# def spam2(apple=green,)
+
+# all_products = 'Склад'{'name':'Хлеб','Количество':34}
+# def get_products(a):
+#     print(all products['Склад'][a])
+# get_products()
+
+# def spam1(*args):
+#     return args
+# print(spam1(1,2,3,'Hallo'))
+
+# def spam1(**kwargs):
+#     return kwargs
+# print(spam1(name=my))
+
+# def spam1(**kwargs):
+#    fro k,v in kwargs.items():
+#     return k,v
+#     print(spam1(name='my1',age=23))
+
+ # def nums (5,7,8,9,10)
+ # a = int()
+ # if user
+ # elif
+ #
+ #
+ # while True
+
+# def sorter(a):
+#     if a % 2 == 0:
+#         print(f'Число{а}четное')
+#     else:
+#         print(f'Число{а}нечетное')
+#     While True:
+#     b = input ('Введите число:')
+#     sorter(b)
+# import requests
+# response = requests.get(https://afisha.yandex.ru/moscow?utm_source=ntp_chrome)
+
+
+
+# x = lambda e:e
+# print e
+#
+# x = lambda a:a**2
+# print (a(4))
+
+# map - встреная функуция.
+#
+# x = [1,2,3,"4"]
+# a = map(lambda d: d*2,x)
+# print(list(a))
+#
+#
+# x = [1,2,3,"4"]
+#
+# I = ['']
+
+# x = [1,2,-4,-4,-8,-7,4,15,18,19,21,98]
+# y = filter (lambda d:d>0,x)
+# print(list(y))
+
+# a = range (1,100,2)
+# print (type(a))
+# print(a)
+# print (list(a))
+
+
+# list1 = [1,2,3]
+# tuple1 = (1,2,3)
+# dict1 = {1:2,3:4}
+# str1 = "123"
+# int1 = 123
+# float = 10.5
+#
+#
+# print(len())
+# float1 = 2.3157647823964
+# print(round(float1,3))
+# round- кол-во цифр после запятой
+
+# def name ():
+#     return 1
+# l = [1,2,3]
+# b = map(name(a),l)
+# print(b)
+# class User:
+#     def_init_(self,name):
+
+
+# class chat:
+#     def_init_(username,text,likes):
+#     self.username = username
+#     self.text = text
+#     self.likes = likes
+
+# class_Car:
+# def_init_(self,model,color,year):
+#     self.model = model
+#     self.color = color
+#     self.year = year
+#
+# def stop(self):
+#     print(f'{self} остановилась')
+#
+# model = input
+# gentra =
+
+
+
+# class Car:
+#     def_init_(self,model,color,year):
+#         self.model = model
+#         self.color = color
+#         self.year = year
+#
+# def stop(self):
+#     print('Машина остановилась')
+# def change_color (self,new_color):
+#     self.color(self,new_color):
+#
+# gentra = Car('Ravon','Black',2022)
+#
+# gentra.change_color('White')
+# gentra.color
+
+# class Bank:
+# class BankAccount:
+#     def __init__(self, name, balance=0.0):
+#         self.name = name
+#         self.balance = balance
+#     def deposite (sefl,summa):
+#         self.balance = self.balance + summa
+#     def cash(self,summa)
+#         if self.balance >=summa:
+#            self.balance = self.balance - summa
+#            print(f'С баланса снято')
+#
+#             print(f'Недостаточно средств')
+#
+#
+#
+#
+#     def my_balance(self):
+# print(self.balance)
+
+
+git
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
